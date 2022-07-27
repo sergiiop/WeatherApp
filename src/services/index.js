97bc82de2813b7c154649dbb9758b9713eecd37a
Sergio Pérez <sergio-luis98@hotmail.com>
2022-07-27 14:21:12 -0500

    adding https url

--- src/services/index.js
+++ src/services/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const baseUrl = 'http://api.weatherapi.com/v1/current.json?key=7135fb54f7d849828d100245222607&q='
+const baseUrl = 'https://api.weatherapi.com/v1/current.json?key=7135fb54f7d849828d100245222607&q='
 
 export const getOneCity = async (city) => {
   const res = await axios.get(`${baseUrl}${city}&aqi=yes`)
